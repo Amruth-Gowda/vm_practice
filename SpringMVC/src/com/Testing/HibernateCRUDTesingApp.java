16426892b2ddbec40bc43a358faefb68ff7b1844
Amruth-Gowda <gowdaamruth01@gmail.com>
2019-12-13 12:29:21 +0530

    hibernate util creation

--- SpringMVC/src/com/Testing/HibernateCRUDTesingApp.java
+++ SpringMVC/src/com/Testing/HibernateCRUDTesingApp.java
@@ -10,66 +10,45 @@ public class HibernateCRUDTesingApp {
 	public static void main(String[] args) {
 
 		EmployeeDAO dao = new EmployeeDAO();
+		
+		//read the list of employees
+		List<Employee> emplList = dao.getEmployees();
+		System.out.println("list of employees: ");
+		for (Employee employee : emplList) {
+			System.out.println(employee);
+		}
 
-//		// insert data into to the database
-//		Employee employee = new Employee("xyz", "male", "abc@abc.com", "9898998425");
-//		dao.addEmployee(employee);
+		// insert data into to the database
+		Employee employee = new Employee("navred", "female", "v@m.com", "9898998425");
+		dao.addEmployee(employee);
 
-//		//read individual data from the database
-//		Employee employee2 = dao.getEmployee(1);
-//		System.out.println("Employee Detailes: " + employee2);
-
-//		// read multiple data from the database
-//		List<Employee> emplList = dao.getEmployees();
-//		for (Employee employee3 : emplList) {
-//			System.out.println("In the Main app: " + employee3);
-//		}
+//		// read individual data from the database
+//		Employee employee = dao.getEmployee(13);
+//		System.out.println("Employee Detailes: " + employee);
 
 //		// update the employee details
-//		int empId = 1;
+//		int empId = 13;
 //		Employee employee = dao.getEmployee(empId);
-//		System.out.println("Current employee details are: " + employee);
-//
-//		employee.setEmp_name("amruth");
-//		employee.setEmp_mail("amruth@amruth.com");
+//		employee.setEmp_name("naveena");
+//		employee.setEmp_mail("naveena@naveena.com");
 //		if (dao.updateEmployee(employee) == 1)
 //			System.out.println("updated successfully");
 //		else
 //			System.out.println("update failed.");
-//		
-//		Employee employee2 = dao.getEmployee(empId);
-//		System.out.println("Updated employee details are: " + employee2);
-
-//		//delete an empployee
-//		List<Employee> empList = dao.getEmployees();
-//		System.out.println("Employee list before for deleting: ");
-//		for(Employee employee: empList) {
-//			System.out.println(employee);
-//		}
-//		
-//		Employee employee = dao.getEmployee(3);
+		
+//		//delete an employee by id
+//		Employee employee = dao.getEmployee(6);
 //		System.out.println("Deleting employee details: "+employee);
 //		dao.deleteEmployee(employee);
-//		
-//		List<Employee> empList1 = dao.getEmployees();
-//		System.out.println("Employee list after for deleting: ");
-//		for(Employee employee1: empList1) {
-//			System.out.println(employee1);
-//		}
+		
+//		//delete multiple employees through patterns
+//		dao.deleteEmployeesBasedOnPattern("%bc.com");
 
-		// delete multiple employees on id basis
-		List<Employee> empList = dao.getEmployees();
-		System.out.println("Employee list before for deleting: ");
-		for (Employee employee : empList) {
-			System.out.println(employee);
-		}
-
-		dao.deleteEmployeesBasedOnPattern("%xy.com");
-
-		List<Employee> empList1 = dao.getEmployees();
-		System.out.println("Employee list after for deleting: ");
-		for (Employee employee1 : empList1) {
-			System.out.println(employee1);
+		// read multiple data from the database
+		List<Employee> emplList1 = dao.getEmployees();
+		System.out.println("Updated list of employees: ");
+		for (Employee employeeUpdated : emplList1) {
+			System.out.println(employeeUpdated);
 		}
 	}
 }
--- SpringMVC/src/com/util/HibernateUtil.java
+++ SpringMVC/src/com/util/HibernateUtil.java
@@ -1,10 +1,39 @@
 package com.util;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-	public static void createSessionFactory() {
-		SessionFactory factory = new Configuration().configure().buildSessionFactory();
+	private static HibernateUtil hibernateUtil;
+	private static SessionFactory sessionFactory;
+
+	private HibernateUtil() {
+		sessionFactory = new Configuration().configure().buildSessionFactory();
+	}
+
+	public static HibernateUtil getInstance() {
+		if (hibernateUtil == null)
+			hibernateUtil = new HibernateUtil();
+		return hibernateUtil;
+	}
+
+	public Session getSession() {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		return session;
+	}
+
+	public void commitSession(Session session) {
+		session.getTransaction().commit();
+	}
+	
+	public void commitCloseSession(Session session) {
+		session.getTransaction().commit();
+		session.close();
+	}
+	
+	public void closeSession(Session session) {
+		session.close();
 	}
 }
